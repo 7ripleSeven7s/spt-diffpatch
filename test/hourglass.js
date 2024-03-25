2f0947fd3f8d65f7e1c40db502c03da6cfd1cec2
waveryder808 <ohsmith81@gmail.com>
2024-03-24 18:12:21 -0700

    Hackerrank Challenge - hourglass.js

--- test/hourglass.js
+++ test/hourglass.js
@@ -15,32 +15,26 @@ const arraySample = [
  */
 function hourglassSum(arr) {
   // 2D array representing the 16 hourglass possible positions
-  let hourglassLoc2D = [
+/*   let hourglassLoc2D = [
     [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-    [1, 2, 3, 4],
-  ];
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+  ]; */
   // Storage variable for largest hourglass sum out of 16 subtotals.
   //   if (newSum > highestSum) { highestSum = newSum }
   let highestSum = 0;
+  for (let i = 1; i <= 4; i++) {
+    for (let j = 1; j <= 4; j++) {
+      let newSum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]
+                                 + arr[i][j] +
+                   arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
 
-  // 6 x 6 array
-  /*   for (let row of arr) {
-    for (let col of row) {
-
-    }
-  } */
-
-  for (let j = 1; j <= 4; j++) {
-    /* let newSum =
-    arr[j-1][j-1] + arr[j-1][j] + arr[j-1][j+1]
-              + arr[j][j] +
-    arr[j+1][j-1] + arr[j+1][j] + arr[j+1][j+1]; */
-    console.log(newSum);
-    if (newSum > highestSum) {
-      highestSum = newSum;
-      console.log("new high ", highestSum);
+      console.log(newSum);
+      if (newSum > highestSum) {
+        highestSum = newSum;
+        console.log("new high ", highestSum);
+      }
     }
   }
   return highestSum;
@@ -50,10 +44,10 @@ function hourglassSum(arr) {
       0 1 2 3 4 5
     +------------
     0| 1 1 1 0 0 0 
-    1| 0 1 0 0 0 0
+    1| 0 1 0 0 0 0 <-- starting midpoint 1,1
     2| 1 1 1 0 0 0
     3| 0 0 2 4 4 0
-    4| 0 0 0 2 0 0
+    4| 0 0 0 2 0 0   <-- ending midpoint 3,3
     5| 0 0 1 2 4 0 
     -------------
     Every hourglass set-indice shifts 4-right and 4-down

a77a45e2c9cbb8cfff98ed5f4da8a5f6a4e09771
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-04-01 16:00:15 -0700

    working on app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* function Greeting(props) {
+  return <h1>{props.text}</h1>;
+}
 
+const items = [{},{},[1,2,3]];
+
+{ items.map(itm => (
+  <Component key={itm.id} />
+)) } */
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}

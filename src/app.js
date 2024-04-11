d868718ceebfdfc4affa844a5a96b838675801b8
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-04-11 06:58:16 -0700

    added code to app.js. moved stuff to object-clonging.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,12 @@
-/* function Greeting(props) {
-  return <h1>{props.text}</h1>;
+"use strict";
+
+// *** Imports ***
+
+
+function checkProps(obj1, obj2) {
+  return {
+    
+  }
 }
 
-const items = [{},{},[1,2,3]];
 
-{ items.map(itm => (
-  <Component key={itm.id} />
-)) } */
-
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
-
-  return (
-    <ul>{listItems}</ul>
-  );
-}
--- test/object-cloning.js
+++ test/object-cloning.js
@@ -58,3 +58,29 @@ console.log(objClone);
     '3': { name: 'John', age: 27, degree: 'IT' }
   }
  */
+
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+export default function ShoppingList() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "magenta" : "darkgreen",
+      }}>
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+const items = [{}, {}, [1, 2, 3]];
+
+{
+  items.map((itm) => <Component key={itm.id} />);
+}

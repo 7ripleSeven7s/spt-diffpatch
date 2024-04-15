e10f65609849d5b8bb84de46b0053162fcbcd4af
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-04-15 15:10:01 -0700

    restructured clone.js

--- test/clone.js
+++ test/clone.js
@@ -1,80 +1,33 @@
 "use strict";
 
-/* Object keys & values */
-
-const obj1 = {
-  name: "John",
-  age: 21,
-  degree: "CS",
-};
+const obj1 = { name: "John", age: 21, degree: "CS" };
 const obj2 = { name: "Robert", age: 43 };
 const obj3 = { name: "Max", age: 23, degree: "CS" };
 const obj4 = { name: "John", age: 27, degree: "IT" };
 
-if (obj3.age !== undefined) {
-  console.log(obj3.name, " has age of: " + obj3.age);
-}
-prompt("hello");
-
-console.table({
-  obj1,
-  obj2,
-  obj3,
-  obj4,
-});
-
 const objects = [obj1, obj2, obj3, obj4];
-let objCustom = {a:"hi",b:123};
-/* ***  for..in  ***/
+//console.table([...objects]);
 
-const student = {
-  name: "John",
-  age: 21,
-  degree: "CS",
-};
+const student = { name: "John", age: 21, degree: "CS" };
+const student2 = {};
 
 // Clone object using for..in loop
-const student2 = {};
-for (const key in student) {
-  student2[key] = student[key];
-  console.log(
-    `${key}: ${student[key]}, ${student2[key]}, ===:${
-      student[key] === student2[key]
-    }, Object.is():${Object.is(student, student2)}`
-  );
-}
+const weakClone = (tgt, src) => {
+  let key = {};
+};
 
-const student3 = [...student];
-console.log(
-  `object destructuring: '===':${student === student3}, 'Object.is():${Object.is(
-    student,
-    student3
-  )}'`
-);
-/* 
-  [
-    { name: "John", age: 21, degree: "CS" },
-    { name: "Robert", age: 43 },
-    { name: "Max", age: 23, degree: "CS" },
-    { name: "John", age: 27, degree: "IT" },
-  ]; 
-*/
+// Clone via Object destructuring
+const student3 = { ...student };
+console.log(`object destructuring:`);
+console.log(`===:${student === student3}`);
+console.log(`Object.is():${Object.is(student, student3)}`);
 
-const objClone = { ...objects };
-console.log(objClone);
-/* 
-  {
-    '0': { name: 'John', age: 21, degree: 'CS' },
-    '1': { name: 'Robert', age: 43 },
-    '2': { name: 'Max', age: 23, degree: 'CS' },
-    '3': { name: 'John', age: 27, degree: 'IT' }
-  }
- */
-
+// Object Array
 const products = [
   { title: "Cabbage", isFruit: false, id: 1 },
   { title: "Garlic", isFruit: false, id: 2 },
   { title: "Apple", isFruit: true, id: 3 },
 ];
 
+// Mixed Object Arrays
 const items = [{}, {}, [1, 2, 3]];

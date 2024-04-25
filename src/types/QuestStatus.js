18bf8b17f130ea7486fac6c24d895b01a3beab5b
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-04-24 21:21:01 -0700

    added MUCH more type information, moved to src/ folder

---
+++ src/types/QuestStatus.js
@@ -0,0 +1,13 @@
+export const QuestStatus = Object.freeze(
+{
+    Locked : 0,
+    AvailableForStart : 1,
+    Started : 2,
+    AvailableForFinish : 3,
+    Success : 4,
+    Fail : 5,
+    FailRestartable : 6,
+    MarkedAsFailed : 7,
+    Expired : 8,
+    AvailableAfter : 9,
+});

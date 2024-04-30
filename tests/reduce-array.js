a35b154839ce166ae9a6089f8d089d1d1b711989
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-04-30 11:24:39 -0700

    folder restructure

--- tests/reduce-array.js
+++ tests/reduce-array.js
@@ -4,7 +4,7 @@
  * Reduce an Array without duplicates *
  **************************************/
 
-const oldArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+/* const oldArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const obj = {};
 
@@ -27,17 +27,20 @@ const arrayWithDuplicates2 = [1, 2, 2, 3, 4, 4, 5];
 const uniqueArray2 = arrayWithDuplicates2.filter((value, index, self) => {
   return self.indexOf(value) === index;
 });
-console.log(uniqueArray2); // Output: [1, 2, 3, 4, 5]
+console.log(uniqueArray2); // Output: [1, 2, 3, 4, 5] */
 
 // The reduce() method executes a reducer function on each element of the array, resulting in a single output value.
 // By combining it with includes(), we can efficiently accumulate unique values.
 
 const array3 = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray3 = array3.reduce((accumulator, currentValue) => {
-  return accumulator.includes(currentValue)
-    ? accumulator
-    : [...accumulator, currentValue];
-}, []);
+const uniqueArray3 = array3.reduce(
+  (accumulator, currentValue) => {
+    return accumulator.includes(currentValue)
+      ? accumulator
+      : [...accumulator, currentValue];
+  },
+  [array3[array3.length]]
+);
 console.log(uniqueArray3); // Output: [1, 2, 3, 4, 5]
 
 //Pros: Offers a high degree of control and is highly readable. Suitable for more complex deduplication logic.

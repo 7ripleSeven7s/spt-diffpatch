0ca396dda3cb580e326d97d79e2bc9874037afe7
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-05-13 03:04:11 -0700

    resorting

---
+++ config/exports.js
@@ -0,0 +1,136 @@
+export const Traders = Object.freeze(
+{
+    PRAPOR : "54cb50c76803fa8b248b4571",
+    THERAPIST : "54cb57776803fa99248b456e",
+    FENCE : "579dc571d53a0658a154fbec",
+    SKIER : "58330581ace78e27b8b10cee",
+    PEACEKEEPER : "5935c25fb3acc3127c3d8cd9",
+    MECHANIC : "5a7c2eca46aef81a7ca2145d",
+    RAGMAN : "5ac3b934156ae10c4430e83c",
+    JAEGER : "5c0647fdd443bc2504c2d371",
+    LIGHTHOUSEKEEPER : "638f541a29ffd1183d187f57",
+});
+
+export const BOT_ROSTER_GLOBAL = [
+  "assault", 
+  "bear", 
+  "bossboar",
+  "bossboarsniper",
+  "bossbully",
+  "bossgluhar",
+  "bosskilla",
+  "bossknight",
+  "bosskojaniy",
+  "bosskolontay",
+  "bosssanitar",
+  "bosstagilla",
+  "bosszryachiy",
+  "crazyassaultevent",
+  "cursedassault",
+  "exusec",
+  "followerbigpipe",
+  "followerbirdeye",
+  "followerboar",
+  "followerboarclose1",
+  "followerboarclose2",
+  "followerbully",
+  "followergluharassault",
+  "followergluharscout",
+  "followergluharsecurity",
+  "followergluharsnipe",
+  "followerkojaniy",
+  "followerkolontayassault",
+  "followerkolontaysecurity",
+  "followersanitar",
+  "followertagilla",
+  "followerzryachiy",
+  "marksman",
+  "peacefullzryachiyevent",
+  "pmcbot",
+  "ravangezryachiyevent",
+  "sectactpriestevent",
+  "sectantpriest",
+  "sectantwarrior",
+  "shooterbtr",
+  "usec"
+];
+    
+export const BOT_SQUAD_CUSTOMS = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "bossbully",
+  "followerbully",
+];
+
+export const BOT_SQUAD_WOODS = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "bosskojaniy",
+  "followerkojaniy",
+  "pmcbot"
+];
+
+export const BOT_SQUAD_FACTORY = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "bosstagilla",
+  "followertagilla"
+];
+
+export const BOT_SQUAD_INTERCHANGE = [
+  "assault",
+  "usec",
+  "bear",
+  "bosskilla" 
+];
+
+export const BOT_SQUAD_SHORELINE = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "bosssanitar",
+  "followersanitar"
+];
+
+export const BOT_SQUAD_RESERVE = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "pmcbot",
+  "bossgluhar",
+  "followergluharassault",
+  "followergluharscout",
+  "followergluharsecurity"
+];
+
+export const BOT_SQUAD_LABS = [
+  "usec",
+  "bear",
+  "pmcbot"
+]; 
+
+export const BOT_SQUAD_LIGHTHOUSE =  [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+  "exusec",
+  "bossknight",
+  "followerbigpipe",
+  "followerbirdeye"
+];
+
+export const BOT_SQUAD_STREETS = [
+  "assault",
+  "usec",
+  "bear",
+  "marksman",
+];

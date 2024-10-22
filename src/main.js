cbb8ea97b167f2a90b872c87477a04fe4f63d376
7ripleSeven7s <7ripleSeven7s@gmail.com>
2024-10-21 20:07:09 -0700

    Updated dependencies - node@23.0.0, npm@10.9

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var process = require("node:process");
 var fs = require("node:fs");
 var path = require("node:path/posix");
-// *** File Path Enums ***
+
+// *** FILE PATH CONSTANTS ***
 var ENV_PATH = process.cwd();
 var CONFIG_PATH = path.join(ENV_PATH, '/config/app.json');
 var DATA_PATH = path.join(ENV_PATH, 'data/');
 var IMAGES_PATH = path.join(ENV_PATH, 'images/');
 var STYLE_PATH = path.join(ENV_PATH, 'style/');
 var TEST_PATH = path.join(ENV_PATH, 'test/');
+
 // #region Testing
 var e = 'world';
 var addOrConcat = function (a, b, c) {
@@ -17,8 +19,11 @@ var addOrConcat = function (a, b, c) {
         return a + b;
     return '' + a + b;
 };
+
 var myVal = addOrConcat(2, 2, 'concat');
 var nextVal = addOrConcat(2, 2, 'add');
+
+// Logging environment variables
 console.log(CONFIG_PATH);
 console.log(DATA_PATH);
 console.log(IMAGES_PATH);
@@ -26,6 +31,7 @@ console.log(STYLE_PATH);
 console.log(TEST_PATH);
 console.log(myVal);
 console.log(nextVal);
+
 // #endregion Testing
 try {
     if (!fs.existsSync(CONFIG_PATH)) {
